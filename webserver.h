fb4457471fc418335c432599150ec3f4d9d4ffc3
anlixia <578520080@qq.com>
2022-06-08 22:32:36 +0800

    add webserver.cpp

--- webserver.h
+++ webserver.h
@@ -12,13 +12,69 @@
 #include <cassert>
 #include <sys/epoll.h>
 
+#include "./threadpool/threadpool.h"
+#include "./http/http_conn.h"
 
+const int MAX_FD = 65536; // 最大文件描述符
+const int MAX_EVENT_NUMBER = 10000;  // 最大事件数
+const int TIMESLOT = 5;  //最小超时单位
 
 class WebServer {
 public:
     WebServer();
     ~WebServer();
 
+    void init(int port, string user, string password, string databaseName, 
+            int log_write, int opt_linger, int trigmode, int sql_num,
+            int thread_num, int close_log, int actor_model);
+    
+    void thread_pool();
+    void sql_pool();
+    void log_write();
+    void trig_mode();
+    void eventListen();
+    void eventLoop();
+    void timer(int connfd, struct sockaddr_in client_address);
+    void adjust_timer(util_timer *timer);
+    void deal_timer(util_timer *timer, int sockfd);
+    bool dealclientdata();
+    bool dealwithsignal(bool& timeout, bool& stop_server);
+    void dealwithread(int sockfd);
+    void dealwithwrite(int sockfd);
+
+public:
+    int m_port;
+    char *m_root;
+    int m_log_write;
+    int m_close_log;
+    int m_actormodel;
+
+    int m_pipefd[2];
+    int m_epollfd;
+    http_conn *users;
+
+    // DB
+    connection_pool *m_connPool;
+    string m_user;
+    string m_passWord;
+    string m_databaseName;
+    int m_sql_num;
+
+    // thread pool
+    threadpool<http_conn> *m_pool;
+    int m_thread_num;
+
+    // epoll_event
+    epoll_event events[MAX_EVENT_NUMBER];
+
+    int m_listenfd;
+    int m_OPT_LINGER;
+    int m_LISTENTrigmode;
+    int m_CONNTrigmode;
+
+    // 定时器
+    client_data *users_timer;
+    Utils utils;
 };
 
 #endif
