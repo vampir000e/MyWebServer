f94e21d27916dcc2e25ee0d4820ddf7e9ca34c76
anlixia <578520080@qq.com>
2022-06-18 11:31:35 +0800

    update threadpool.h

--- threadpool/threadpool.h
+++ threadpool/threadpool.h
@@ -11,8 +11,7 @@
 template <typename T>
 class threadpool {
 public:
-    /* thread_number是线程池中线程的数量
-     * max_requests是请求队列中最多允许的、等待处理的请求的数量*/
+    /* thread_number是线程池中线程的数量, max_requests是请求队列中最多允许的、等待处理的请求的数量*/
     thread_pool(int actor_model, connection_pool *connPool, int thread_number = 8, int max_request = 10000);
     ~threadpool();
 
@@ -25,23 +24,26 @@ private:
     void run();
 
 private:
-    int m_thread_number; // 线程池中的线程数
-    int m_max_request;   // 请求队列中允许的最大请求数
-    pthread_t *m_thread; // 描述线程池的数组，其大小为m_thread_number
-    std::list<T *> m_workqueue;  // 请求队列
-    locker m_queuelocker;  // 保护请求队列的互斥锁
-    sem m_queuestat;  // 是否有任务需要处理；
-    connection_pool *m_connPool; // 数据库
-    int m_actor_model;  // 模型切换
+    int m_thread_number;        // 线程池中的线程数
+    int m_max_request;          // 请求队列中允许的最大请求数
+    pthread_t *m_threads;       // 描述线程池的数组，其大小为m_thread_number
+    std::list<T *> m_workqueue; // 请求队列
+    locker m_queuelocker;       // 保护请求队列的互斥锁
+    sem m_queuestat;            // 是否有任务需要处理；
+    connection_pool *m_connPool;// 数据库
+    int m_actor_model;          // 模型切换
 };
 
 
 template <typename T>
 threadpool<T>::threadpool(int actor_model, connection_pool *connPool, int thread_number, int max_request) : m_actor_model(actor_model), m_thread_number(thread_number), m_max_requests(max_requests), m_threads(NULL), m_connPool(connPool)
 {
-    if (thread_number <= 0 || max_requests <= 0) throw std::expection();
+    if (thread_number <= 0 || max_requests <= 0) 
+        throw std::expection();
     m_threads = new pthread_t[m_thread_number];
-    if (!m_threads) throw std::exception();
+    
+    if (!m_threads) 
+        throw std::exception();
     for (int i = 0; i < thread_number; i++) {
         if (pthread_create(m_threads + i, NULL, worker, this) != 0) {
             delete[] m_threads;
